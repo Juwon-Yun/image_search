0c3d8dc2f083dea7e19cc0eec1c00bcd07d98abf
Juwon-Yun <eooll2@daum.net>
2022-07-20 12:45:35 +0900

    feature: provider package

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:search_image_app/data/photo_api_repositor.dart';
 import 'package:search_image_app/data/photo_provider.dart';
 import 'package:search_image_app/ui/main_screen.dart';
 import 'package:search_image_app/ui/main_view_model.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(
@@ -17,8 +18,12 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: PhotoProvider(
-        mainViewModel: MainViewModel(repository: PixabayApi()),
+      // home: PhotoProvider(
+      //   mainViewModel: MainViewModel(repository: PixabayApi()),
+      //   child: MainScreen(),
+      // ),
+      home: Provider(
+        create: (_) => MainViewModel(repository: PixabayApi()),
         child: MainScreen(),
       ),
     );
