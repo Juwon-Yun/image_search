a17e526f9c2779c8c57c00048c68b75c70339846
Juwon-Yun <eooll2@daum.net>
2022-10-18 21:20:49 +0900

    - update: ChangeNotifier, 가변배열

--- lib/ui/main_view_model.dart
+++ lib/ui/main_view_model.dart
@@ -1,19 +1,29 @@
 import 'dart:async';
 
+import 'package:flutter/material.dart';
 import 'package:search_image_app/data/photo_api_repository.dart';
 import 'package:search_image_app/models/photo.dart';
 
 // api -> repository로 바뀌면서 관계를 없앴다.
-class MainViewModel {
+class MainViewModel with ChangeNotifier {
   final PhotoApiRepository repository;
+
+  List<Photo> _photos = [];
+
+  List<Photo> get photos => _photos;
 
   MainViewModel({required this.repository});
 
-  final _photoScreenController = StreamController<List<Photo>>()..add([]);
-  Stream<List<Photo>> get photoStream => _photoScreenController.stream;
+  // final _photoScreenController = StreamController<List<Photo>>()..add([]);
+  // Stream<List<Photo>> get photoStream => _photoScreenController.stream;
 
+  // Future<void> fetch(String query) async {
+  //   final result = await repository.fetchImageWithQuery(query);
+  //   _photoScreenController.add(result);
+  // }
   Future<void> fetch(String query) async {
     final result = await repository.fetchImageWithQuery(query);
-    _photoScreenController.add(result);
+    _photos = result;
+    notifyListeners();
   }
 }
