042c71b69f49d59644913ce02f01c90e1280b56c
Juwon-Yun <eooll2@daum.net>
2022-10-24 21:26:45 +0900

    - update: photoApi use case 정의

---
+++ lib/domain/use_case/get_photos_use_case.dart
@@ -0,0 +1,15 @@
+import 'package:search_image_app/data/data_source/result_freezed.dart';
+import 'package:search_image_app/domain/model/photo.dart';
+import 'package:search_image_app/domain/repository/photo_api_repository.dart';
+
+// use_case 에서는 viewModel 에서 하는 데이터를 가져오고 가공하는 로직을 담는다.
+// viewModel 에서는 하지 않아야 클린 아키텍처.
+class GetPhotosUseCase {
+  final PhotoApiRepository repository;
+
+  GetPhotosUseCase({required this.repository});
+
+  Future<Result<List<Photo>>> execute(String query) async {
+    return await repository.fetchImageWithQuery(query);
+  }
+}
