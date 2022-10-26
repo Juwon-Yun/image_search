9b1ecd94e6e42605d134bd1a58c78cdc68139245
Juwon-Yun <eooll2@daum.net>
2022-10-26 22:41:35 +0900

    - update: main 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:search_image_app/data/repositor/photo_api_repositor.dart';
-import 'package:search_image_app/domain/use_case/get_photos_use_case.dart';
+import 'package:search_image_app/di/provider_setup.dart';
 import 'package:search_image_app/presentation/home/main_screen.dart';
-import 'package:search_image_app/presentation/home/main_view_model.dart';
 import 'package:provider/provider.dart';
 
 void main() {
+  // FIXME: refactor v5
   runApp(
-    const MainApp(),
+    MultiProvider(
+      providers: globalProviders,
+      child: const MainApp(),
+    ),
   );
+
+  // runApp(
+  //   const MainApp(),
+  // );
 }
 
 class MainApp extends StatelessWidget {
@@ -26,16 +32,18 @@ class MainApp extends StatelessWidget {
       // FIXME: refactor v5
       // 여기서 의존성 주입하는 로직을 별도로 작성하는게 좀 더 좋다.
       // 확장성을 고려하자
-      home: ChangeNotifierProvider(
-        // FIXME: refactor v3
-        // create: (_) => MainViewModel(repository: PixabayApi()),
+      home: const MainScreen(),
 
-        // FIXME: refactor v4
-        create: (_) => MainViewModel(
-          getPhotosUseCase: GetPhotosUseCase(repository: PixabayApi()),
-        ),
-        child: MainScreen(),
-      ),
+      // home: ChangeNotifierProvider(
+      //   // FIXME: refactor v3
+      //   // create: (_) => MainViewModel(repository: PixabayApi()),
+      //
+      //   // FIXME: refactor v4
+      //   create: (_) => MainViewModel(
+      //     getPhotosUseCase: GetPhotosUseCase(repository: PixabayApi()),
+      //   ),
+      //   child: MainScreen(),
+      // ),
     );
   }
 }
